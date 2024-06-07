29ccd57688571c9329cd16f5e1f7b99015287802
korius kussa wenda <koriuswenda@gmail.com>
2024-06-07 17:53:48 +0700

    feat : file index dan main

---
+++ main.js
@@ -0,0 +1,21 @@
+// 1. variable - conts
+// 2. tipe data - tipe string
+const str1 = "Korius Wenda";
+
+
+// string objects
+const str2 = 'KORIUS WENDA';
+
+// string 
+const str3 = `${str2}`;
+
+// console
+// function console
+
+console.log(str1.toUpperCase());
+console.log(str2.toLowerCase());
+console.log(str3);
+
+
+
+
